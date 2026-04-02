a25e25cc764bc3d215b3dd541f49f931ed2c7367
soufiane2001 <67438275+soufiane2001@users.noreply.github.com>
2026-04-02 23:36:29 +0300

    Update Projects.tsx

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -85,7 +85,7 @@ const projects: Project[] = [
     image: "https://res.cloudinary.com/dzkx1z6lo/image/upload/v1775161310/Capture_d_%C3%A9cran_2026-04-02_231851_senzhp.png",
     demoUrl: "https://gamlastanshop.space/",
     tech: ["nextjs"],
-  },
+  }
 ];
 
 const Projects = () => {
